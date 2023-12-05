62e55d542e9be6de57429e20300d585c47638855
sblack <snb1582@gmail.com>
2023-12-04 22:01:47 -0500

    Added Bing Search API, now must test

--- server.js
+++ server.js
@@ -2,75 +2,98 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 const pdfParse = require('pdf-parse');
-const { PDFDocument } = require('pdf-lib');
 const mammoth = require('mammoth');
-const { Document, Packer, Paragraph } = require('docx');
-
+const natural = require('natural');
+const sentenceTokenizer = new natural.SentenceTokenizer();
+const tokenizer = new natural.WordTokenizer();
+const axios = require('axios');
 const upload = multer({ dest: 'uploads/' });
 const app = express();
 app.use(cors());
 
-function proofreadText(text) {
-    let changes = [];
-    let lines = text.split('\n');
+async function contextualProofread(text) {
+    const apiKey = '###'; // Replace with your Bing API Key
+    const endpoint = 'https://api.bing.microsoft.com/'; // Replace with your Bing API Endpoint
 
-    lines = lines.map((line, index) => {
-        if (line.includes('typrrro')) {
-            let updatedLine = line.replace(/typrrro/g, 'typo');
-            changes.push(`Line ${index + 1}: Replaced 'typrrro' with 'typo'`);
-            return updatedLine;
+    try {
+        const params = new URLSearchParams({ 
+            'text': text,
+            'mode': 'proof' // Use 'proof' mode for more rigorous checks
+        });
+
+        const headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Ocp-Apim-Subscription-Key': apiKey
+        };
+
+        const response = await axios.post(endpoint, params.toString(), { headers });
+        
+        // Process the response here
+        // The response format will depend on the API documentation
+        return response.data;
+    } catch (error) {
+        console.error('Error calling Bing Spell Check API:', error);
+        return text; // Fallback to original text or basic spell checking
+    }
+}
+
+async function proofreadText(text) {
+    let changes = [];
+    let sentences = sentenceTokenizer.tokenize(text);
+
+    const proofreadSentences = await Promise.all(sentences.map(async (sentence, index) => {
+        const response = await contextualProofread(sentence);
+        
+        // Process the response to apply corrections and record changes
+        // Simplified example: Adjust according to the actual API response format
+        let correctedSentence = response.correctedText; // Assuming the API returns the corrected sentence
+        if (correctedSentence !== sentence) {
+            changes.push(`Sentence ${index + 1} corrected.`);
         }
-        return line;
-    });
+        return correctedSentence;
+    }));
 
     return {
-        proofreadText: lines.join('\n'),
+        proofreadText: proofreadSentences.join(' '),
         changes: changes
     };
-}
-
-async function createPDF(text) {
-    const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage();
-    page.drawText(text);
-    return pdfDoc.save();
-}
-
-async function createWordDocument(text) {
-    const doc = new Document();
-    const paragraphs = text.split('\n').map(line => new Paragraph(line));
-    doc.addSection({ children: paragraphs });
-    return Packer.toBuffer(doc);
 }
 
 app.post('/upload', upload.single('file'), async (req, res) => {
     const file = req.file;
     const fileType = file.mimetype;
 
-    if (fileType === 'application/pdf') {
-        const dataBuffer = fs.readFileSync(file.path);
-        try {
+    try {
+        let text;
+        if (fileType === 'application/pdf') {
+            const dataBuffer = fs.readFileSync(file.path);
             const data = await pdfParse(dataBuffer);
-            const result = proofreadText(data.text);
-            const pdfBytes = await createPDF(result.proofreadText);
-            res.send({ fileType: 'pdf', data: pdfBytes.toString('base64'), changes: result.changes });
-        } catch (error) {
-            console.error('Error processing PDF:', error);
-            res.status(500).send('Error processing PDF');
-        }
-    } else if (fileType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
-        try {
+            text = data.text;
+        } else if (fileType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
             const data = await mammoth.extractRawText({ path: file.path });
-            const result = proofreadText(data.value);
-            const docBuffer = await createWordDocument(result.proofreadText);
-            res.send({ fileType: 'docx', data: docBuffer.toString('base64'), changes: result.changes });
-        } catch (error) {
-            console.error('Error processing Word document:', error);
-            res.status(500).send('Error processing Word document');
+            text = data.value;
+        } else {
+            res.status(400).send('Unsupported file type');
+            return;
         }
-    } else {
-        res.status(400).send('Unsupported file type');
+
+        // Proofreading the extracted text
+        const result = await proofreadText(text);
+        
+        // Saving the proofread text as a .txt file
+        const outputFilePath = `uploads/proofread_${file.originalname}.txt`;
+        fs.writeFileSync(outputFilePath, result.proofreadText);
+
+        res.send({ 
+            fileType: 'txt', 
+            filePath: outputFilePath, 
+            changes: result.changes 
+        });
+    } catch (error) {
+        console.error('Error processing file:', error);
+        res.status(500).send('Error processing file');
     }
 });
 
